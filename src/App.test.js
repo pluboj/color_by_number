1f9d4c68d879c789901454a512a25727f96426fb
plubojacky <plubojacky@olsonresearchca.com>
2018-11-30 10:35:11 -0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('App', () => {
+  it('should have the `th` "test"', () => {
+    const wrapper = shallow(
+      <App />
+    );
+    expect(
+      wrapper.contains(<th>test</th>)
+    ).toBe(true);
+  });
+})
